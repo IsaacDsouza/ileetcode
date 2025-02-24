def71a68013dc824501edcc52830f8e45e7d5187
IsaacDsouza <isaacmdsouza@gmail.com>
2025-02-24 23:21:19 +0530

    Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.

---
+++ ne1603.py
@@ -0,0 +1,11 @@
+#1603. Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.spaces = [big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        if self.spaces[carType - 1]>0:
+            self.spaces[carType - 1] -= 1
+            return True
+        return False
