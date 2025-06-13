707f663983fc0f3746f98482a9a4e3f22d1f57c8
IsaacDsouza <isaacmdsouza@gmail.com>
2025-06-14 00:26:48 +0530

    206. Reverse a linked list.

---
+++ ne206.py
@@ -0,0 +1,40 @@
+#206. Reverse a linked list.
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def __str__(self):
+        result = []
+        curr = self
+        while curr:
+            result.append(str(curr.val))
+            curr = curr.next
+        return "->".join(result)
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            temp = curr.next
+            curr.next=prev
+            prev=curr
+            curr = temp
+        return prev
+def build_linked_list(values):
+    if not values:
+        return None
+    head = ListNode(values[0])
+    curr = head
+    for val in values[1:]:
+        curr.next = ListNode(val)
+        curr = curr.next
+    return head
+
+solution = Solution()
+linked_list_head = build_linked_list([1, 2, 3, 4, 5])
+reversed_head = solution.reverseList(linked_list_head)
+print(reversed_head) 
