93737cfb03e0d600409b6c94a8a6827df9100440
IsaacDsouza <isaacmdsouza@gmail.com>
2025-06-30 12:50:50 +0530

    200. Number of Islands

--- ne200.py
+++ ne200.py
@@ -2,20 +2,24 @@
 
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
-        d=[(1,0),(0,1),(-1,0),(0,-1)]
-        def dfs(i,j):
-            if 0<=i<len(grid) and 0<=j<len(grid[0]) and grid[i][j]=="1":
-                grid[i][j]=0
-                for dr,dc in d:
-                    nr,nc=i+dr,j+dc
-                    dfs(nr,nc)
-        cnt=0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=="1":
-                    cnt+=1
+        m,n = len(grid), len(grid[0])
+        def dfs(i, j):
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j]!='1':
+                return 
+            else:
+                grid[i][j]='0'
+                dfs(i,j+1)
+                dfs(i+1,j)
+                dfs(i,j-1)
+                dfs(i-1,j)
+        islands=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=='1':
+                    islands+=1
                     dfs(i,j)
-        return cnt
+        return islands
+      
 
 solution=Solution()
 print(solution.numIslands([["1","1","1","1","0"],
