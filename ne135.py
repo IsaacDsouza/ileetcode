11805b6ae378fd6c01d54c3abed9e0e6676bf640
IsaacDsouza <isaacmdsouza@gmail.com>
2025-07-28 17:14:21 +0530

    135. Candy.

---
+++ ne135.py
@@ -0,0 +1,19 @@
+#135. Candy. There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings. You are giving candies to these children subjected to the following requirements: Each child must have at least one candy. Children with a higher rating get more candies than their neighbors. Return the minimum number of candies you need to have to distribute the candies to the children.
+
+class Solution:
+    def candy(self, ratings: list[int]) -> int:
+        arr=[1]*len(ratings)
+
+        for i in range(1, len(ratings)): #comparing left neighbour
+            if ratings[i]>ratings[i-1]:
+                arr[i]=arr[i-1]+1
+
+        for i in range(len(ratings)-2,-1,-1): #comparing right neighbour
+            if ratings[i]>ratings[i+1]:
+                arr[i]=max(arr[i], arr[i+1]+1)
+        return sum(arr)
+
+
+
+solution = Solution()
+print(solution.candy([1,2,2]))
