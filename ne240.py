92c3905c6d04a57e48db501e2be0fc80dcc7425e
Isaac Marian Dsouza <isaacmdsouza@gmail.com>
2025-09-13 15:42:06 +0530

    240. Search a 2D Matrix.

---
+++ ne240.py
@@ -0,0 +1,19 @@
+#240. Search a 2D Matrix II. Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties: Integers in each row are sorted in ascending from left to right. Integers in each column are sorted in ascending from top to bottom.
+
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+
+        i, j = rows-1, 0
+
+        while i>=0 and j<cols:
+            if matrix[i][j]==target:
+                return True
+            elif matrix[i][j]<target:
+                j+=1
+            else:
+                i-=1
+        return False
+
+solution = Solution()
+print(solution.searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 30))
